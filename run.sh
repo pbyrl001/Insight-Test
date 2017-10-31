162115368c6d30402a5b2a165c04480bed40a8fe
pbyrl001 <33243724+pbyrl001@users.noreply.github.com>
2017-10-31 00:43:54 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,28 @@
-#!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
-#python ./src/find_political_donors.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
+#change txt to csv file for easier minupulation
+import csv
+import pandas as pd
+
+txt_file = r"mytestfile.txt"
+csv_file = r"input.csv"
+
+in_txt = csv.reader(open(txt_file, "r+"), delimiter = '|')
+out_csv = csv.writer(open(csv_file, 'wb'))
+
+out_csv.writerows(in_txt)
+
+#parse information and create median values by zip file
+
+df = pd.read_csv('input.csv', header=None)
+
+df = df.groupby(11).agg({0:'first', 15:'median', 16:'first'})
+
+df.to_csv(r'medianvals_by_zip.txt', header=None, sep='|', mode='a')
+
+#parse information and create median values by date file
+
+df = pd.read_csv('input.csv', header=None)
+
+df = df.groupby(14).agg({0:'first', 15:'median', 16:'first'})
+
+df.to_csv(r'medianvals_by_date.txt', header=None, sep='|', mode='a')
+
